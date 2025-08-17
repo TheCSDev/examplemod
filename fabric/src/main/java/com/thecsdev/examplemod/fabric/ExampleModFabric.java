7432d4ae0681bd2a269426fa85799fbd6684fef6
TheCSDev <thecsdev@gmail.com>
2025-08-17 03:30:05 +0200

    Minor versioning tweaks

--- fabric/src/main/java/com/thecsdev/examplemod/fabric/ExampleModFabric.java
+++ fabric/src/main/java/com/thecsdev/examplemod/fabric/ExampleModFabric.java
@@ -4,11 +4,17 @@ import com.thecsdev.examplemod.fabric.client.ExampleModFabricClient;
 import com.thecsdev.examplemod.fabric.server.ExampleModFabricServer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 
-public final class ExampleModFabric implements ClientModInitializer, DedicatedServerModInitializer
+public final class ExampleModFabric implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer
 {
 	// ==================================================
-	public @Override void onInitializeClient() { new ExampleModFabricClient(); }
-	public @Override void onInitializeServer() { new ExampleModFabricServer(); }
+	public final @Override void onInitializeClient() { new ExampleModFabricClient(); }
+	public final @Override void onInitializeServer() { new ExampleModFabricServer(); }
+	// --------------------------------------------------
+	public final @Override void onInitialize()
+	{
+		//TODO - Place Fabric-only common initialization code here
+	}
 	// ==================================================
 }
